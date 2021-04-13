0385a2de8c8d04ae1f77fae1f450c23a3dd4992f
A1cor <tal1403@gmail.com>
2021-04-13 19:04:49 +0300

    requirements.txt update UserRating model added Video and Tagging models upgraded ForeignKeys logic added TaggingValidator created (Will be tested later)

--- videos/models.py
+++ videos/models.py
@@ -1,15 +1,42 @@
 from django.db import models
 from django.forms import ModelForm
 from embed_video.fields import EmbedVideoField
-from datetime import datetime
+import datetime
+from django.contrib.auth.models import User
+
+date = datetime.date(1, 1, 1)
+MAX_START_TO_END_RANGE = datetime.datetime.combine(date, datetime.time(0, 5, 0)) - \
+                         datetime.datetime.combine(date, datetime.time(0, 0, 0))
+
 
 class Video(models.Model):
     video = EmbedVideoField()  # same like models.URLField()
+    length = models.TimeField()
 
+
+class UserRating(models.Model):
+    creator = models.ForeignKey(User, related_name='creator_user_rating', on_delete=models.CASCADE)
+    tagging = models.ForeignKey(User, related_name='tagging_user_rating', on_delete=models.CASCADE)
+    is_upvote = models.BooleanField()
+
+
+class TaggingValidator:
+    @classmethod
+    def get_errors(cls, creator, video, start, end, date_subscribed, description, rating_value):
+        errors = []
+        time_range = datetime.datetime.combine(date, start) - datetime.datetime.combine(date, end)
+        if datetime.datetime.combine(date, start) < datetime.datetime.combine(date, datetime.time(0, 0, 0)) or \
+                datetime.datetime.combine(date, end) > datetime.datetime.combine(date, video.length):
+            errors.append(f'Time out of range')
+        if time_range > MAX_START_TO_END_RANGE:
+            errors.append(f'Invalid time range, Maximal valid time range duration is {MAX_START_TO_END_RANGE}')
 
 
 class Tagging(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
     start = models.TimeField()
     end = models.TimeField()
-    date_subscribed = models.DateTimeField(default=datetime.now())
-    description = models.TextField(max_length=200)
+    date_subscribed = models.DateTimeField(default=datetime.datetime.now())
+    description = models.TextField(max_length=50)
+    rating_value = models.IntegerField(default=0)
