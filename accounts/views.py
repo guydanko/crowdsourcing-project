e8d48addb31601d6ac64123be2f2e1fb9cf72359
guydanko <guy.dankovich@gmail.com>
2021-04-13 11:22:21 +0300

    working UI for login and logout flow

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
@@ -18,16 +19,16 @@ def register(request):
                     user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
                     user.save()
                     messages.success(request, 'User was created successfully')
-                    return redirect('login')
+                    return redirect('accounts:login')
                 else:
                     messages.error(request, 'Email already exists')
-                    return redirect('register')
+                    return redirect('accounts:register')
             else:
                 messages.error(request, 'Username already exists')
-                return redirect('register')
+                return redirect('accounts:register')
         else:
             messages.error(request, 'Passwords do not match')
-            return redirect('register')
+            return redirect('accounts:register')
     else:
         return render(request, 'accounts/register.html')
 
@@ -41,23 +42,21 @@ def login(request):
             auth.login(request, user)
             # can be used with: {% if user.is_authenticated %}
             messages.success(request, f'Welcome {username}')
-            return redirect('dashboard')
+            return redirect('layout:index')
         else:
             messages.error(request, 'Invalid credentials')
-            return redirect('login')
+            return redirect('accounts:login')
     else:
         return render(request, 'accounts/login.html')
 
 
+@login_required(login_url='accounts:login')
 def logout(request):
-    if request.method == 'POST':
-        auth.logout(request)
-        messages.success(request, 'You are now logged out')
-    return redirect('register')
+    auth.logout(request)
+    messages.success(request, 'You are now logged out')
+    return redirect('layout:index')
 
 
-def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
 
 
 
